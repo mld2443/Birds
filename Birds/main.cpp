4c88e035558d833ec6f60d2f1f3782e0aeac817c
Matthew Dillard <7395051@gmail.com>
2015-10-15 02:09:38 -0500

    Initial Commit

---
+++ Birds/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Birds
+//
+//  Created by Matthew Dillard on 10/15/15.
+//  Copyright © 2015 Matthew Dillard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
